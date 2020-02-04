88bfd74fe23cf00bfb9791df755598ca21a10a1d
Majd Imar <majd2012995@gmail.com>
2020-02-05 00:18:41 +0200

    new script

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var hi = prompt("what is your age ?");
+console.log(hi);
+var you; 
+ 
+if (hi > 18 && hi<35   ) {     
+    you = 'you prefer modern sites';   
+} 
+else if (hi > 12 && hourNow <=18) {     
+    you = 'you prefer diseny land';   
+  
+} else {     
+    you = 'you prefer historical places';   
+} 
+
+document.write('<h3>'+you+'</h3>');
