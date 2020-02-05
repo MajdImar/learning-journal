379e913cabd862affc9e3dc653a3f711b95aece1
Majd Imar <majd2012995@gmail.com>
2020-02-05 11:57:29 +0200

    nweone

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ var hi = prompt("what is your age ?");
 console.log(hi);
 var you; 
  
-if (hi > 18 &&hi<35   ) {     
+if (hi > 18 && hi<35   ) {     
     you = 'you prefer modern sites';   
 } 
-else if (hi > 12 && hourNow <=18) {     
+else if (hi > 12 && hi <=18) {     
     you = 'you prefer diseny land';   
   
 } else {     
