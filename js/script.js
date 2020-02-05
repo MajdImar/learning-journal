7017c51053ab6fb3a854e0528a868fb2c1e9b2b5
Majd Imar <majd2012995@gmail.com>
2020-02-05 12:33:33 +0200

    func

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 var hi = prompt("what is your age ?");
 console.log(hi);
 var you; 
+function  hello (hi){
+
  
-if (hi > 18 && hi<35   ) {     
+if (hi > 18 && hi < 35   ) {     
     you = 'you prefer modern sites';   
 } 
 else if (hi > 12 && hi <=18) {     
@@ -11,5 +13,6 @@ else if (hi > 12 && hi <=18) {
 } else {     
     you = 'you prefer historical places';   
 } 
-
-document.write('<h3>'+you+'</h3>');
+return (you);
+}
+document.write('<h3>'+hello(hi)+'</h3>') ; 
