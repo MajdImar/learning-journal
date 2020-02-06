28f0d62d506a8ab1260d5c4dc46847c10e972640
Majd Imar <majd2012995@gmail.com>
2020-02-06 05:09:28 +0200

    finaly

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+var tn=  prompt ("what is your name ? ") ;
+var mj =  prompt ("do you like walking ? ") ;
+console .log(mj);
 var hi = prompt("what is your age ?");
 console.log(hi);
+while(hi >70 || hi< 4) {
+    hi=prompt('please enter your age')
+}
 var you; 
-function  hello (hi){
 
- 
+
+function  hello (hi){
 if (hi > 18 && hi < 35   ) {     
     you = 'you prefer modern sites';   
 } 
@@ -15,4 +21,30 @@ else if (hi > 12 && hi <=18) {
 } 
 return (you);
 }
-document.write('<h3>'+hello(hi)+'</h3>') ; 
+document.write('<h3>'+hello(hi)+'</h3>') ;
+
+function tour () {
+    var prf = prompt ("which places do you prefer?( modern or hostrical" ) ; 
+while (prf !=='modern' && prf !=='hostrical') {
+    prf = prompt("plz enter  modern or hostrical" );
+}
+
+    var places=prompt("how many places do you prefer to visit?") ;
+    console .log (places) ;
+    var num = "";
+    
+    for(var i=0 ; i < places ; i=+1) {
+    if(prf==='Modern' || prf==='modern') {
+    num= num + '<img src="https://www.urtrips.com/wp-content/uploads/2018/01/The-Boulevard-Amman-5.jpg">' ;
+} else if(prf==='hostrical') {
+    num = num +'<img src="https://cdn.thecrazytourist.com/wp-content/uploads/2017/03/Petra.jpg"alt="petra">'  ;
+}
+else {
+    num ='undifiend'
+}
+return  num;
+}
+
+}
+document.write('<p>'+tour()+'</p>') ;
+
